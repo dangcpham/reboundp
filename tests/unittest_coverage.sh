40964753a342e4dedea197afdc29b918bb20a349
Dang Pham <dcp237@cornell.edu>
2023-12-05 16:22:07 -0500

    test rebound3 compatability

--- tests/unittest_coverage.sh
+++ tests/unittest_coverage.sh
@@ -3,7 +3,11 @@ coverage erase
 pip uninstall --y urllib3 rebound
 coverage run -m -a unittest test_environment/test_features_base.py -v
 
-pip install urllib3 rebound
+pip install 'rebound<4.0.0'
+coverage run -m -a unittest test_environment/test_features_rebound3.py -v
+pip uninstall --y rebound
+
+pip install urllib3 'rebound>=4.0.0'
 coverage run -m -a unittest discover -s . -v
 coverage run -m -a unittest test_environment/test_features_port.py -v
 
--- tests/unittest_run_all.sh
+++ tests/unittest_run_all.sh
@@ -1,6 +1,10 @@
 pip uninstall --y urllib3 rebound
 python -m unittest test_environment/test_features_base.py -v
 
-pip install urllib3 rebound
+pip install 'rebound<4.0.0'
+python -m unittest test_environment/test_features_rebound3.py -v
+pip uninstall --y rebound
+
+pip install urllib3 'rebound>=4.0.0'
 python -m unittest discover -s . -v
 python -m unittest test_environment/test_features_port.py -v
