a8fbf2da1fec973697caf6d6614783407d586f34
Dang Pham <dcp237@cornell.edu>
2023-12-05 19:54:40 -0500

    another try

--- tests/unittest_coverage.sh
+++ tests/unittest_coverage.sh
@@ -8,7 +8,8 @@ coverage run -m -a unittest test_environment/test_features_rebound3.py -v
 pip uninstall --y rebound
 
 pip install urllib3 'rebound>=4.0.0'
-coverage run -m -a unittest test_environment/test_features_port.py -v
 coverage run -m -a unittest discover -s . -v
+coverage run -m -a unittest test_environment/test_features_port.py -v
+
 
 coverage html -d coverage_report
