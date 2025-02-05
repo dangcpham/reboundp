a59bd1ac69ac229db01aebd3103ef9e7e72586b4
Dang Pham <dcp237@cornell.edu>
2025-02-05 03:18:49 -0500

    update test instructions

--- tests/unittest_run_all.sh
+++ tests/unittest_run_all.sh
@@ -1,10 +1,10 @@
-pip uninstall --y urllib3 rebound
-python -m unittest test_environment/test_features_base.py -v
+# pip uninstall --y urllib3 rebound
+# python -m unittest test_environment/test_features_base.py -v
 
-pip install 'rebound<4.0.0'
-python -m unittest test_environment/test_features_rebound3.py -v
-pip uninstall --y rebound
+# pip install 'rebound<4.0.0'
+# python -m unittest test_environment/test_features_rebound3.py -v
+# pip uninstall --y rebound
 
-pip install urllib3 'rebound>=4.0.0'
+pip install urllib3 'rebound>=4.0.0' flask
 python -m unittest discover -s . -v
-python -m unittest test_environment/test_features_port.py -v
+# python -m unittest test_environment/test_features_port.py -v
